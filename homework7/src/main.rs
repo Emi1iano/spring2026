59fa9dda5306ac95ca75da6a57b68bc70d1902ff
Emi1iano <150746114+Emi1iano@users.noreply.github.com>
2026-04-15 23:56:07 +0000

    homework 7

---
+++ homework7/src/main.rs
@@ -0,0 +1,35 @@
+use std::thread;
+use std::time::Duration;
+
+fn assignment1() {
+    // TODO: Create a vector to store thread handles
+    let mut handles = vec![];
+    
+    // TODO: Spawn 3 threads
+    for i in 1..=3 {
+        // TODO: Spawn a thread and store its handle
+        let handle = thread::spawn(move || {
+            // Simulate some work
+            println!("Thread {} starting", i);
+            thread::sleep(Duration::from_millis(500));
+            println!("Thread {} finished", i);
+        });
+        
+        // TODO: Store the handle
+        handles.push(handle);
+    }
+    
+    // TODO: Wait for all threads to complete
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+fn assignment2() {
+
+}
+fn main() {
+    println!("Main thread starting");
+    assignment1();
+    assignment2();
+    println!("All threads completed.");
+}
