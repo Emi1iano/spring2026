f9086812b59ec1581dcffb2e305f7371c791fc28
Emi1iano <150746114+Emi1iano@users.noreply.github.com>
2026-04-16 00:05:21 +0000

    homework 7

--- homework7/src/main.rs
+++ homework7/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::{Arc, Mutex};
 
 fn assignment1() {
     // TODO: Create a vector to store thread handles
@@ -25,7 +26,36 @@ fn assignment1() {
     }
 }
 fn assignment2() {
-
+    // TODO: Create a shared counter using Arc and Mutex
+    let counter = Arc::new(Mutex::new(0));
+    
+    // TODO: Create a vector to store thread handles
+    let mut handles = vec![];
+    
+    // TODO: Spawn 5 threads
+    for _ in 1..=5 {
+        // TODO: Clone the Arc for the thread
+        let counter_clone = Arc::clone(&counter);
+        
+        // TODO: Spawn a thread that increments the counter 10 times
+        let handle = thread::spawn(move || {
+            // TODO: Increment counter 10 times
+            for _ in 0..10 {
+                let mut num = counter_clone.lock().unwrap();
+                *num += 1;
+            }
+        });
+        
+        handles.push(handle);
+    }
+    
+    // TODO: Wait for all threads to complete
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    
+    // TODO: Print the final value of the counter
+    println!("{}", *counter.lock().unwrap());
 }
 fn main() {
     println!("Main thread starting");
